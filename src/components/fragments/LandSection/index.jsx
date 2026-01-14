6328daf74a6acc0af907ba23584b7a5d9ca7b6fe
Dava Elyanta <dava@example.com>
2026-01-14 09:25:10 +0700

    pindhan

--- src/components/fragments/LandSection/index.jsx
+++ src/components/fragments/LandSection/index.jsx
@@ -1,23 +1,54 @@
-import React from 'react';
+import React from "react";
 import Typo from "@/components/atoms/TypoGrafis";
 import LandCard from "@/components/molecules/LandCard";
+import { fetchLahan } from "@/services/lahan";
 
 const LandSection = () => {
+  const [lahanList, setLahanList] = fetchLahan();
+
+  const data = async () => {
+    try{
+      const lahanData = await fetchLahan();
+      setLahanList(lahanData);
+      console.log("Lahan data fetched:", lahanData);
+    }catch(err){
+      console.error("Error fetching lahan data:", err);
+    }
+  }
+
   return (
     <section className="px-4 md:px-10 py-4">
-      <Typo Variant="h2" ClassName="mb-4 text-base md:text-lg">Prototype Lahan</Typo>
-      
-      <div className="flex h-[220px] gap-4 overflow-x-auto relative horizontal-scroll pb-6 no-scrollbar snap-x">
+      <Typo Variant="h2" ClassName="mb-4 text-base md:text-lg">
+        Prototype Lahan
+      </Typo>
+
+      <div className="flex h-[270px] gap-4 overflow-x-auto relative pb-6 no-scrollbar snap-x">
         <LandCard isNew />
-        <LandCard 
-          image="/images/sawah-2.jpg" 
-          title="Self Resource" 
-          subtitle="Kangkung, Tomat, Cabai" 
-        />
-        <LandCard 
+        {lahanList.map((item, index) => (
+          <LandCard
+            key={"dummy"}
+            image={"dummy"}
+            title={"dummy"}
+            subtitle={"item.subtitle"}
+            meta={{
+              tanaman: "item.tanaman",
+              hektar: "item.hektar",
+              tanah: "item.tanah",
+              createdAt: "item.createdAt",
+            }}
+          />
+        ))}
+
+        <LandCard
           image="/images/sawah-1.jpg"
-          title="Summer Template" 
-          subtitle="Padi, Jagung" 
+          title="Summer Template"
+          subtitle="Padi, Jagung"
+          meta={{
+            tanaman: "2 Jenis",
+            hektar: "5 Ha",
+            tanah: "Alluvial",
+            createdAt: "5 Feb 2026",
+          }}
         />
       </div>
     </section>
